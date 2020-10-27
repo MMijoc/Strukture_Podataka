3b8fb03451bb8a4e8f92b9596f6b2edd62aa7ee5
MMijoc <markomijoc18@gmail.com>
2020-10-27 15:30:36 +0100

    dodane funkcije: SelectMenu, PrintError i Input from user

--- SP_lab_2/SP_lab_2/Source.c
+++ SP_lab_2/SP_lab_2/Source.c
@@ -36,6 +36,10 @@ int PrintList(person head);
 int PrintPerson(person *P);
 int FindByLastName(person head, char *lastName);
 int RemovePerson(person *head, char *lastName);
+int PrintMenu();
+int SelectMenu();
+int PrintError(char *errorMessage);
+person *InputFromUser();
 
 
 int main()
@@ -45,21 +49,25 @@ int main()
 	int isError = 0;
 
 
+	person *tmp = NULL;
+	tmp = InputFromUser();
+	PrintPerson(tmp);
 
-	isError = InsertAtHead(&Head, CreateNewNode("Dennis", "Ritchie", 1941));
-	isError = InsertAtHead(&Head, CreateNewNode("Christopher", "Tolkien", 1924));
-	isError = InsertAtHead(&Head, CreateNewNode("Piet", "Mondrian", 1872));
-	isError = InsertAtHead(&Head, CreateNewNode("Gordon", "Freeman", 1972));
-	isError = InsertAtHead(&Head, CreateNewNode("Christoph", "Waltz", 1956));
-	isError = InsertAtHead(&Head, CreateNewNode("Joe", "Satriani", 1956));
-	isError = InsertAtHead(&Head, CreateNewNode("Bruce", "Dickinson", 1958));
-	isError = InsertAtHead(&Head, CreateNewNode("Marko", "Mijoc", 1999));
-	isError = InsertAtTail(&Head, CreateNewNode("Irene", "Jansen", 1983));
-	isError = InsertAtHead(&Head, CreateNewNode("Floor", "Jansen", 1981));
-	
-	if (isError) return FAILURE;
 
-	PrintList(Head);
+	//isError = InsertAtHead(&Head, CreateNewNode("Dennis", "Ritchie", 1941));
+	//isError = InsertAtHead(&Head, CreateNewNode("Christopher", "Tolkien", 1924));
+	//isError = InsertAtHead(&Head, CreateNewNode("Piet", "Mondrian", 1872));
+	//isError = InsertAtHead(&Head, CreateNewNode("Gordon", "Freeman", 1972));
+	//isError = InsertAtHead(&Head, CreateNewNode("Christoph", "Waltz", 1956));
+	//isError = InsertAtHead(&Head, CreateNewNode("Joe", "Satriani", 1956));
+	//isError = InsertAtHead(&Head, CreateNewNode("Bruce", "Dickinson", 1958));
+	//isError = InsertAtHead(&Head, CreateNewNode("Marko", "Mijoc", 1999));
+	//isError = InsertAtTail(&Head, CreateNewNode("Irene", "Jansen", 1983));
+	//isError = InsertAtHead(&Head, CreateNewNode("Floor", "Jansen", 1981));
+	//
+	//if (isError) return FAILURE;
+
+	//PrintList(Head);
 
 
 	return SUCCESS;
@@ -212,5 +220,83 @@ int PrintMenu()
 	printf("\n\tExit -> Izlaz iz programa\n");
 	printf("\nIzbor: ");
 
+	return SUCCESS;
+}
+
+int SelectMenu()
+{
+	char select[BUFFER_LENGTH] = { '\0' };
+	int isError = 0;
+	person Head = { .name = "HEAD",.lastName = "HEAD",.birthYear = 0,.next = NULL };
+
+	while (true) {
+		if (isError) return FAILURE;
+		if (_stricmp(select, "0") == 0) {
+
+
+		}
+		else if (strcmp(select, "1") == 0) {
+
+		}
+		else if (strcmp(select, "2") == 0) {
+
+		}
+		else if (strcmp(select, "3") == 0) {
+
+		}
+		else if (strcmp(select, "4") == 0) {
+
+		}
+		else if (strcmp(select, "5") == 0) {
+
+		}
+		else if (_stricmp(select, "exit") == 0) {
+			break;
+		}
+		else {
+			printf("Nepoznata naredba \"%s\"", select);
+			continue;
+		}
+
+	}
+
+	return SUCCESS;
+}
+
+person *InputFromUser()
+{
+	char name[BUFFER_LENGTH];
+	char lastName[BUFFER_LENGTH];
+	int birthYear = 0;
+	person *newPerson = NULL;
+
+	printf("Unesite ime: ");
+	fgets(name, BUFFER_LENGTH - 1, stdin);
+	strtok(name, "\n");
+	printf("Unesite prezime: ");
+	fgets(lastName, BUFFER_LENGTH - 1, stdin);
+	strtok(lastName, "\n");
+	printf("Unesite godinu rodjenja: ");
+	scanf(" %d", &birthYear);
+
+	newPerson = CreateNewNode(name, lastName, birthYear);
+	if (newPerson == NULL) {
+		PrintError(NULL);
+		return NULL;
+	}
+
+	return newPerson;
+}
+
+int PrintError(char *errorMessage)
+{
+
+	if (errorMessage == NULL) {
+		fprintf(stderr, "\nUnknown Error!");
+		return SUCCESS;
+	} else {
+		fprintf(stderr, errorMessage);
+
+	}
 	return SUCCESS;
 }
