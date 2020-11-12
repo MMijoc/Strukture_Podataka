b04a49de029c52954ced505247a5881a976c0f59
MMijoc <markomijoc18@gmail.com>
2020-11-12 14:32:43 +0100

    Added: DeleteList, PrintMenu and SelectMenu; Cleaned up code

--- SP_lab_4/SP_lab_4/Source.c
+++ SP_lab_4/SP_lab_4/Source.c
@@ -22,42 +22,128 @@ typedef struct _node {
 	struct _node *next;
 } node;
 
+
+int PrintMenu();
+int SeectMenu();
+int PrintList(node *head);
 node *CreateNewNode(double coeff, int exp);
+int InsertAtPosition(node *position, node *nodeToInsert);
 int SortedInput(node *head, node *nodeToInsert);
-int PrintError(char *message);
 int InputFromFile(node *head, char *fileName);
 int InputFromString(node *head, char *buffer);
-int PrintList(node *head);
-int InsertAtPosition(node *position, node *nodeToInsert);
 int AddPoly(node *poly1, node *poly2, node *result);
 int MultiplyPoly(node *poly1, node *poly2, node *result);
+int DeleteList(node *head);
+int ConsoleInput(char *message, char *buffer, size_t bufferSize);
+int PrintError(char *message);
+
 
 int main()
+{
+
+	return SeectMenu();
+}
+
+int PrintMenu()
+{
+	printf("1 -> Unesi prvi polinom iz datoteke\n");
+	printf("2 -> Unesi drugi polinom iz datoteke\n");
+	printf("3 -> Zbroji polinome\n");
+	printf("4 -> Pomnozi polinome\n");
+	printf("\texit -> izlaz iz programa\n");
+
+	return SUCCESS;
+}
+
+int SeectMenu()
 {
 	char tmpBuffer[BUFFER_LENGTH];
 	node poly1 = {0, 0, NULL};
 	node poly2 = {0, 0, NULL};
-	node result = {0, 0, NULL};
+	node result1 = {0, 0, NULL};
 	node result2 = {0, 0, NULL};
+	int isError = 0;
 
 
-	strcpy(tmpBuffer, "poly1");
-	InputFromFile(&poly1, tmpBuffer);
+	while (!isError) {
+		isError = PrintMenu();
+		ConsoleInput("Izbor: ", tmpBuffer, BUFFER_LENGTH);
 
-	strcpy(tmpBuffer, "poly2");
-	InputFromFile(&poly2, tmpBuffer);
-
-	PrintList(&poly1);
-	PrintList(&poly2);
-
-	AddPoly(&poly1, &poly2, &result);
-	PrintList(&result);
-
-	MultiplyPoly(&poly1, &poly2, &result2);
-	PrintList(&result2);
-
+		if (strcmp(tmpBuffer, "1") == 0) {
+			ConsoleInput("Unesite ime datoteke u kojoj je zapisan prvi polinom: ", tmpBuffer, BUFFER_LENGTH);
+			if (poly1.next != NULL)
+				DeleteList(&poly1);
+			InputFromFile(&poly1, tmpBuffer);
+		
+		} else if (strcmp(tmpBuffer, "2") == 0) {
+			ConsoleInput("Unesite ime datoteke u kojoj je zapisan drugi polinom: ", tmpBuffer, BUFFER_LENGTH);
+			if (poly2.next != NULL)
+				DeleteList(&poly2);
+			InputFromFile(&poly2, tmpBuffer);
+		
+		} else if (strcmp(tmpBuffer, "3") == 0) {
+			if (poly1.next == NULL || poly2.next == NULL) {
+				printf("Potrebno je unijeti oba polinoma!\n");
+				ConsoleInput("Press any key to continue . . .", tmpBuffer, BUFFER_LENGTH);
+				system("cls");
+				continue;
+			}
+			isError = AddPoly(&poly1, &poly2, &result1);
+			printf("Prvi polinom: ");
+			PrintList(&poly1);
+			printf("Drugi polinom: ");
+			PrintList(&poly2);
+			printf("Rezultat zbrajanja: ");
+			PrintList(&result1);
+		
+		} else if (strcmp(tmpBuffer, "4") == 0) {
+			if (poly1.next == NULL || poly2.next == NULL) {
+				printf("Potrebno je unijeti oba polinoma!\n");
+				ConsoleInput("Press any key to continue . . .", tmpBuffer, BUFFER_LENGTH);
+				system("cls");
+				continue;
+			}
+			isError = MultiplyPoly(&poly1, &poly2, &result2);
+			printf("Prvi polinom: ");
+			PrintList(&poly1);
+			printf("Drugi polinom: ");
+			PrintList(&poly2);
+			printf("Rezultat mnozenja: ");
+			PrintList(&result2);
+		
+		
+		} else if (_strcmpi(tmpBuffer, "exit") == 0) {
+			return SUCCESS;
+		
+		} else {
+			printf("\nNepoznata naredba \"%s\"", tmpBuffer);
+		}
+		
+		ConsoleInput("Press any key to continue . . .", tmpBuffer, BUFFER_LENGTH);
+		system("cls");
+		DeleteList(&result1);
+		DeleteList(&result2);
 	
-	system("pause");
+	}
+
+
+	return FAILURE;
+}
+
+int PrintList(node *head)
+{
+	node *tmp = head->next;
+
+	while (tmp) {
+		printf("%.2lf*X^(%d)", tmp->coeff, tmp->exp);
+		tmp = tmp->next;
+		if (tmp == NULL) break;
+
+		(tmp->coeff > 0) ? printf(" +") : printf(" ");
+
+	}
+	puts("");
+
 	return SUCCESS;
 }
 
@@ -76,6 +162,15 @@ node *CreateNewNode(double coeff, int exp)
 	tmp->next = NULL;
 
 	return tmp;
+}
+
+int InsertAtPosition(node *position, node *nodeToInsert)
+{
+	
+	nodeToInsert->next = position->next;
+	position->next = nodeToInsert;
+
+	return SUCCESS;
 }
 
 int SortedInput(node *head, node *nodeToInsert)
@@ -161,43 +256,6 @@ int InputFromString(node *head, char *buffer)
 	return SUCCESS;
 }
 
-int PrintError(char *message)
-{
-	if(message == NULL) {
-		fprintf(stderr, "\nUnknown error occurred!");
-		return SUCCESS;
-	}
-	fprintf(stderr, "\n%s", message);
-	
-	return SUCCESS;
-}
-
-int InsertAtPosition(node *position, node *nodeToInsert)
-{
-	
-	nodeToInsert->next = position->next;
-	position->next = nodeToInsert;
-
-	return SUCCESS;
-}
-
-int PrintList(node *head)
-{
-	node *tmp = head->next;
-
-	while (tmp) {
-		printf("%.2lf*X^(%d)", tmp->coeff, tmp->exp);
-		tmp = tmp->next;
-		if (tmp == NULL) break;
-
-		(tmp->coeff > 0) ? printf(" +") : printf(" ");
-
-	}
-	puts("");
-
-	return SUCCESS;
-}
-
 int AddPoly(node *poly1, node *poly2, node *result)
 {
 	poly1 = poly1->next;
@@ -248,5 +306,41 @@ int MultiplyPoly(node *poly1, node *poly2, node *result)
 		tmp = poly2->next;
 	}
 
+	return SUCCESS;
+}
+
+int DeleteList(node *head)
+{
+	node *toFree = NULL;
+	node* tmp = head->next;
+	while(tmp) {
+		toFree = tmp;
+		tmp = tmp->next;
+		free(toFree);
+	}
+
+	head->next = NULL;
+	return SUCCESS;
+}
+
+int ConsoleInput(char *message, char *buffer, size_t bufferSize)
+{
+	if (message)
+		printf("%s", message);
+	memset(buffer, '\0', bufferSize);
+	fgets(buffer, bufferSize - 1, stdin);
+	strtok(buffer, "\n");
+
+	return SUCCESS;
+}
+
+int PrintError(char *message)
+{
+	if(message == NULL) {
+		fprintf(stderr, "\nUnknown error occurred!");
+		return SUCCESS;
+	}
+	fprintf(stderr, "\n%s", message);
+	
 	return SUCCESS;
 }
