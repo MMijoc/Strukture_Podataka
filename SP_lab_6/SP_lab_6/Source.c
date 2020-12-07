e877340bf6d28889aa05c3557de0bf9491f49a62
MMijoc <markomijoc18@gmail.com>
2020-12-07 15:17:01 +0100

    Initial commit

---
+++ SP_lab_6/SP_lab_6/Source.c
@@ -0,0 +1,23 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define SUCCESS 0
+#define FAILURE (-1)
+
+#define BUFFER_LENGTH 1024
+
+
+
+int main()
+{
+	printf("HELLO");
+
+
+	return SUCCESS;
+}
