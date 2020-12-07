6e2bf8e7b3c809471b2fb9107ca7352eabd3c045
MMijoc <markomijoc18@gmail.com>
2020-12-07 15:39:15 +0100

    Implemeted stack; added Puh and Pop

--- SP_lab_6/SP_lab_6/Source.c
+++ SP_lab_6/SP_lab_6/Source.c
@@ -2,6 +2,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
+/*
+Napisati program koji pomocu vezanih listi simulira rad:
+a) stoga,
+b) reda.
+Napomena: Funkcija "push" sprema cijeli broj, slucajno generirani u opsegu od 10 -100.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,12 +19,73 @@
 
 #define BUFFER_LENGTH 1024
 
+typedef struct _node {
+	int value;
 
+	struct _node *next;
+} node;
+typedef node* stack;
+
+node *CreateNewNode(int value);
+int Push(node *stackHead, int value);
+int Pop(node *stackHead, int *result);
 
 int main()
 {
-	printf("HELLO");
+	node Stack = {0, NULL};
+	int i, tmp;
+	for (i = 0; i < 10; i++) {
+		Push(&Stack, i);
+	}
 
+	for (i = 0; i <= 10; i++) {
+		Pop(&Stack, &tmp);
+		printf("%d ", tmp);
+	}
+
+	return SUCCESS;
+}
+
+node *CreateNewNode(int value)
+{
+	node *newNode = NULL;
+
+	newNode = (node *)malloc(sizeof(node));
+	if (NULL == newNode) {
+		perror("Memory allocation failed:");
+		return NULL;
+	}
+
+	newNode->value = value;
+	newNode->next = NULL;
+
+	return newNode;
+}
+
+int Push(node *stackHead, int value)
+{
+	node *nodeToPush = NULL;
+
+	nodeToPush = CreateNewNode(value);
+	if (NULL == nodeToPush) return FAILURE;
+
+	nodeToPush->next = stackHead->next;
+	stackHead->next = nodeToPush;
+
+	return SUCCESS;
+}
+
+int Pop(node *stackHead, int *result)
+{
+	node *nodeToPop = stackHead->next;
+	if (NULL == nodeToPop) {
+		printf("Stack is empty");
+		return FAILURE;
+	}
+
+	*result = nodeToPop->value;
+	stackHead->next = stackHead->next->next;
+	free(nodeToPop);
 
 	return SUCCESS;
 }
