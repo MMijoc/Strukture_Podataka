3e5ba140b7c20837eef0683447b79d830890d799
MMijoc <markomijoc18@gmail.com>
2020-12-29 16:28:32 +0100

    Added basic logic for flow of program; added help cmd command.

--- SP_lab_8/SP_lab_8/Source.c
+++ SP_lab_8/SP_lab_8/Source.c
@@ -1,7 +1,7 @@
 /*
  *Napisati program koji pomocu vezanih listi (stabala) predstavlja strukturu direktorija.
  *Omoguciti unos novih direktorija i pod-direktorija, ispis sadrzaja direktorija i povratak u prethodni direktorij.
- *Tocnije program treba preko menija simulirati koristenje DOS naredbi: 1- "md", 2 - "cd dir", 3 - "cd..", 4 - "dir" i 5 – izlaz.
+ *Tocnije program treba preko menija simulirati koristenje DOS naredbi: 1- "md", 2 - "cd" "dir", 3 - "cd..", 4 - "dir" i 5 – izlaz.
  */
 
 #ifdef _MSC_VER
@@ -13,13 +13,64 @@
 int main()
 {
 
-	return SUCCESS;
+
+	CommandLine();
+	return 	SUCCESS;
 }
 
-
-TreeNode* CreateNewTreeNode()
+int CommandLine()
 {
-	TreeNode* newNode = NULL;
+	TreeNode rootDirectory = { NULL, NULL, NULL };
+	int argumentsTaken = 0;
+	char *inputBuffer = NULL;
+	char *command = NULL;
+	char *commandArgument = NULL;
+	char *path = NULL;
+
+	if (AllocateBuffers(&path, &inputBuffer, &command, &commandArgument) == FAILURE) return FAILURE;
+	SetDirectoryName(&rootDirectory, "C:");
+	strcpy(path, "C:\\");
+
+	while (TRUE) {
+
+		memset(inputBuffer, 0, BUFFER_LENGTH);
+		memset(command, 0, BUFFER_LENGTH);
+		memset(commandArgument, 0, BUFFER_LENGTH);
+
+		printf("%s>", path);
+		fgets(inputBuffer, BUFFER_LENGTH, stdin);
+
+		argumentsTaken = sscanf(inputBuffer, "%s %s", command, commandArgument);
+
+		if (_stricmp(command, "help") == 0) {
+			if (argumentsTaken == 2)
+				PrintHelp(commandArgument);
+			else if (argumentsTaken == 1)
+				PrintHelp(NULL);
+			else
+				printf("\nThis command is not supported by the help utility.\n");
+
+		} else if (_stricmp(command, "cd") == 0) {
+
+		} else if (_stricmp(command, "cd..") == 0) {
+
+		} else if (_stricmp(command, "dir") == 0) {
+
+		} else if (_stricmp(command, "exit") == 0) {
+			return SUCCESS;
+		} else {
+
+
+		}
+
+	}
+
+
+}
+
+TreeNode *CreateNewTreeNode()
+{
+	TreeNode *newNode = NULL;
 
 	newNode = (TreeNode*)malloc(sizeof(TreeNode));
 	if (newNode == NULL) {
@@ -34,13 +85,13 @@ TreeNode* CreateNewTreeNode()
 	return newNode;
 }
 
-StackNode* CreateNewStackNode()
+StackNode *CreateNewStackNode()
 {
-	StackNode* newNode = NULL;
+	StackNode *newNode = NULL;
 
 	newNode = (StackNode*)malloc(sizeof(StackNode));
 	if (newNode == NULL) {
-		PrintError("Tree node memory allocation failed!");
+		PrintError("Stack node memory allocation failed!");
 		return NULL;
 	}
 
@@ -50,9 +101,9 @@ StackNode* CreateNewStackNode()
 	return newNode;
 }
 
-int Push(StackNode* stackHead, TreeNode* treeNode)
+int Push(StackNode *stackHead, TreeNode *treeNode)
 {
-	StackNode* nodeToPush = NULL;
+	StackNode *nodeToPush = NULL;
 
 	nodeToPush = CreateNewStackNode();
 	if (NULL == nodeToPush) return FAILURE;
@@ -65,9 +116,9 @@ int Push(StackNode* stackHead, TreeNode* treeNode)
 	return SUCCESS;
 }
 
-int Pop(StackNode* stackHead, TreeNode *result)
+int Pop(StackNode *stackHead, TreeNode *result)
 {
-	StackNode* nodeToFree = NULL;
+	StackNode *nodeToFree = NULL;
 
 	if (NULL == stackHead->next) {
 		result = NULL;
@@ -82,14 +133,102 @@ int Pop(StackNode* stackHead, TreeNode *result)
 	return SUCCESS;
 }
 
-int PrintError(char* errorMessage)
+int PrintError(char *errorMessage)
 {
 	if (errorMessage == NULL) {
 		fprintf(stderr, "\nUnknown Error!");
 		return SUCCESS;
 	} else {
+		fprintf(stderr, "\n");
 		fprintf(stderr, errorMessage);
 	}
 
 	return SUCCESS;
+}
+
+int AllocateBuffers(char **path, char **inputBuffer, char **command, char **commandArgument)
+{
+	*path = (char *)malloc(BUFFER_LENGTH * sizeof(char));
+	*inputBuffer = (char*)malloc(BUFFER_LENGTH * sizeof(char));
+	*command = (char*)malloc(BUFFER_LENGTH * sizeof(char));
+	*commandArgument = (char*)malloc(BUFFER_LENGTH * sizeof(char));
+	
+	return ValidatePointer(4, "Memory allocation failed: failed to allocate memory for buffers", *path, *inputBuffer, *command, *commandArgument);
+}
+
+int SetDirectoryName(TreeNode *directory, char *directoryName)
+{
+	if (ValidatePointer(2, "Invalid function arguments", directory, directoryName)) return FAILURE;
+
+	directory->directoryName = (char *)malloc(strlen(directoryName) * sizeof(char));
+	if (ValidatePointer(1, "Failed to allocate memory for directory name", directory->directoryName)) return FAILURE;
+
+	strcpy(directory->directoryName, directoryName);
+
+	return SUCCESS;
+}
+
+int ValidatePointer(int numberOfPointersPassed, char *errorMessage, ...)
+{
+	int i = 0, numOfNulls = 0;
+	va_list args;
+
+	va_start(args, errorMessage);
+	for (i = 0; i < numberOfPointersPassed; i++) {
+		void* ptr = va_arg(args, void *);
+		if (ptr == NULL) {
+			numOfNulls++;
+		}
+	}
+	va_end(args);
+
+	if (numOfNulls != 0) {
+		PrintError(errorMessage);
+		fprintf(stderr, " (%d null pointers passed)", numOfNulls);
+		return FAILURE;
+	}
+
+	return SUCCESS;
+}
+
+int PrintHelp(char *command)
+{
+	if (command == NULL) {
+		puts("\nFor more information on a specific command, type HELP command - name"
+			"\nCD             Displays the name of or changes the current directory."
+			"\nDIR            Displays a list of files and subdirectories in a directory."
+			"\nHELP           Provides Help information for Windows commands."
+			"\nMD             Creates a directory.\n"
+		);
+		return SUCCESS;
+
+	} else if (_stricmp(command, "cd") == 0) {
+		puts(	"\nDisplays the name of or changes the current directory.\n"
+			"CD[directory name]\nCD[..]\n"
+			"\n  ..   Specifies that you want to change to the parent directory.\n"
+			"\nType CD without parameters to display the current drive and directory.\n"
+		);
+		return SUCCESS;
+
+	} else if (_stricmp(command, "dir") == 0) {
+		puts("	\nDisplays a list of files and subdirectories in a directory.\n");
+		return SUCCESS;
+
+	} else if (_stricmp(command, "help") == 0) {
+		puts(	"\nProvides help information for Windows commands.\n"
+			"\nHELP [command]\n"
+			"\n    command - displays help information on that command.\n\n"
+		);
+		return SUCCESS;
+
+	} else if (_stricmp(command, "md") == 0) {
+		puts("	\nCreates a directory.\n"	
+			"\nMKDIR [drive:]path\nMD[drive:]path\n\n"
+		);
+		return SUCCESS;
+
+	} else {
+		puts("	\nThis command is not supported by the help utility.\n");
+		return FAILURE;
+	}
 }
--- SP_lab_8/SP_lab_8/Source.h
+++ SP_lab_8/SP_lab_8/Source.h
@@ -3,10 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 #define SUCCESS 0
 #define FAILURE (-1)
 #define BUFFER_LENGTH 1024
+#define TRUE 1
 
 typedef struct _TreeNode {
 	char* directoryName;
@@ -26,4 +28,10 @@ TreeNode* CreateNewTreeNode();
 StackNode* CreateNewStackNode();
 int Push(StackNode* stackHead, TreeNode* treeNode);
 int Pop(StackNode* stackHead, TreeNode* result);
+int AllocateBuffers(char** path, char** inputBuffer, char** command, char** commandArgument);
+int CommandLine();
+int SetDirectoryName(TreeNode* directory, char* directoryName);
+int PrintHelp(char* command);
+
 int PrintError(char* errorMessage);
+int ValidatePointer(int numberOfPointersPassed, char* errorMessage, ...);
