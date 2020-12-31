13f17c1d0834c9b7b3ba15faed4ec0b7d97da277
MMijoc <markomijoc18@gmail.com>
2020-12-31 10:45:29 +0100

    Initial commit

---
+++ SP_lab_10/SP_lab_10/Source.c
@@ -0,0 +1,16 @@
+/*
+ * Napisati program koji iz datoteke cita postfiks izraz i zatim stvara stablo proracuna.
+ * Iz gotovog stabla proracuna upisati u datoteku infiks izraz.
+ */
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include "Source.h"
+
+int main()
+{
+	printf("Hello World\n");
+	return 	SUCCESS;
+}
---
+++ SP_lab_10/SP_lab_10/Source.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SUCCESS 0
+#define FAILURE (-1)
+#define BUFFER_LENGTH 1024
+#define TRUE 1
