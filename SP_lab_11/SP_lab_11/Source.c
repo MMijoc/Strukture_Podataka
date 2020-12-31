c271811650b3c9b516c7a1cdc393fb73908c40a2
MMijoc <markomijoc18@gmail.com>
2020-12-31 20:32:27 +0100

    Initial commit

---
+++ SP_lab_11/SP_lab_11/Source.c
@@ -0,0 +1,20 @@
+/*
+ *Napisati kod koji za zadane podatke studenata (maticni broj, ime i prezime) pravi hash tablicu sa zasebnim redovima.
+ *Tablica ima 11 mjesta, a funkcija za preslikavanje kljuc racuna da se zbraja ASCII vrijednost prvih pet slova prezimena
+ *i zatim racuna ostatak cjelobrojnog dijeljenja te vrijednosti s velicinom tablice.
+ *Studenti s istim kljucem se pohranjuju u vezanu listu sortiranu po prezimenima i imenima.
+ *Kada se tablica izgradi treba ispisati cijelu tablicu (naglasiti na kojem kljucu se nalaze koji podaci)
+ *te ponuditi mogucnost da se za odredjenog studenta (prezime i ime) ispise njegov maticni broj.
+ */
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include "Source.h"
+
+int main()
+{
+	printf("Hello World");
+	return 	SUCCESS;
+}
---
+++ SP_lab_11/SP_lab_11/Source.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SUCCESS 0
+#define FAILURE (-1)
+#define BUFFER_LENGTH 1024
+#define TRUE 1
+#define FALSE 0
