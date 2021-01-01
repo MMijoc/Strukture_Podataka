7d21eba2328905cb17a14b00dedfaf0024569c45
MMijoc <markomijoc18@gmail.com>
2021-01-01 16:31:19 +0100

    Added a way to create, insert data, print and free a hash table

--- SP_lab_11/SP_lab_11/Source.c
+++ SP_lab_11/SP_lab_11/Source.c
@@ -15,6 +15,263 @@
 
 int main()
 {
-	printf("Hello World");
-	return 	SUCCESS;
+	//HashTable hashTable[TABLE_SIZE];
+	//InitHashTable(hashTable, TABLE_SIZE);
+
+
+	HashTable *hashTable = NULL;
+	hashTable = CreateHashTable(TABLE_SIZE);
+	if (hashTable == NULL) return FAILURE;
+
+
+
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Dennis", "Ritchie", "Dennis-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Bjarne", "Stroustrup", "Bjarne-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Linus", "Torvalds", "Linus-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Alan", "Turing", "Alan-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("John Ronald Reuel", "Tolkien", "JRR-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Christopher", "Tolkien", "Christopher-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Floor", "Jansen", "Floor-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Irene", "Jansen", "Irene-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Ahsoka", "Tano", "Ahsoka-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Tommy", "Karevik", "Tommy-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Quentin", "Tarantino", "Quentin-ID"));
+	HashTableInsert(hashTable, TABLE_SIZE, CreateNewPerson("Ardal", "aep Dahy", "Ardal-ID"));
+
+	PrintTable(hashTable, TABLE_SIZE);
+
+	FreeHashTable(hashTable, TABLE_SIZE);
+	return SUCCESS;
+}
+
+Node *CreateNewNode(Person *person)
+{
+	Node *newNode = NULL;
+	
+	newNode = (Node *)malloc(sizeof(Node));
+	if (newNode) {
+		newNode->next = NULL;
+		newNode->person = person;
+		return newNode;
+	} else {
+		PrintError("Memory allocation failed: unable to allocate memory for list node");
+		return NULL;
+	}
+}
+
+Person *CreateNewPerson(char *firstName, char *lastName, char *id)
+{
+	Person *newPerson = NULL;
+
+	newPerson = (Person *)malloc(sizeof(Person));
+	if (newPerson) {
+		strncpy(newPerson->firstName, firstName, NAME_LENGTH);
+		strncpy(newPerson->lastName, lastName, NAME_LENGTH);
+		strncpy(newPerson->id, id, ID_LENGTH);
+		return newPerson;
+	} else {
+		PrintError("Memory allocation failed: unable to allocate memory for person");
+		return NULL;
+	}
+}
+
+HashTable *CreateHashTable(int tableSize)
+{
+	HashTable *newHashTable = NULL;
+	if (tableSize <= 0) {
+		PrintError("Invalid table size!");
+		return NULL;
+	}
+
+	newHashTable = (Node *)malloc(tableSize * sizeof(Node));
+	if (newHashTable) {
+		InitHashTable(newHashTable, tableSize);
+		return newHashTable;
+	} else {
+		PrintError("Memory allocation failed: unable to allocate memory for the hash table");
+		return NULL;
+	}
+}
+
+int InitHashTable(HashTable *table, int tableSize)
+{
+	int i = 0;
+	for (i = 0; i < tableSize; i++) {
+		table[i].person = NULL;
+		table[i].next = NULL;
+	}
+
+	return SUCCESS;
+}
+
+int Hash(char *key, int tableSize)
+{
+	int hashValue = 0;
+	int i = 0;
+
+	if (key == NULL || strlen(key) == 0 || tableSize <= 0)
+		return FAILURE;
+
+	for (i = 0; i < CHARS_HASHING && key[i] != '\0'; i++)
+		hashValue += key[i];
+
+	return hashValue % tableSize;
+}
+
+int HashTableInsert(HashTable *table, int tableSize, Person *toInsert)
+{
+	int hashValue = 0;
+
+	hashValue = Hash(toInsert->lastName, tableSize);
+	if (hashValue < 0) return FAILURE;
+
+	SortedInsert(&(table[hashValue]), toInsert);
+
+	return SUCCESS;
+}
+
+int SortedInsert(Node *listHead, Person *personToInsert)
+{
+	Node *nodeToInsert = NULL;
+	Node *tmp = NULL;
+
+	if (IsNull(2, "Invalid function arguments", listHead, personToInsert) == TRUE) return FAILURE;
+
+	nodeToInsert = CreateNewNode(personToInsert);
+	if (nodeToInsert == NULL) return FAILURE;
+
+	if (listHead->next == NULL) {
+		listHead->next = nodeToInsert;
+		return SUCCESS;
+	}
+
+	tmp = listHead;
+	while (tmp->next != NULL && _stricmp(tmp->next->person->lastName, nodeToInsert->person->lastName) < 0)
+		tmp = tmp->next;
+
+	if (tmp->next != NULL && (_stricmp(tmp->next->person->firstName, nodeToInsert->person->firstName) == 0)) {
+		while (tmp->next != NULL && (_stricmp(tmp->next->person->firstName, nodeToInsert->person->firstName) < 0))
+			tmp = tmp->next;
+
+	}
+
+	nodeToInsert->next = tmp->next;
+	tmp->next = nodeToInsert;
+
+
+	return SUCCESS;
+}
+
+int PrintTable(HashTable *table, int tableSize)
+{
+	int i = 0;
+
+	for (i = 0; i < tableSize; i++) {
+		printf("%d\t", i);
+		PrintList(&table[i]);
+	}
+
+	return SUCCESS;
+}
+
+int PrintList(Node *listHead)
+{
+	Node *tmp = listHead->next;
+
+	if (NULL == tmp) {
+		printf("EMPTY\n");
+		return FAILURE;
+	}
+
+	while (tmp) {
+		PrintPerson(tmp->person);
+		printf("  -->  ");
+		tmp = tmp->next;
+	}
+	printf("\n");
+
+	return SUCCESS;
+}
+
+int PrintPerson(Person *p)
+{
+	printf("\033[31m[%s - %s - %s]\033[0m", p->firstName, p->lastName, p->id);
+	return SUCCESS;
+}
+
+
+
+int FreeHashTable(HashTable *table, int tableSize)
+{
+	int i = 0;
+
+	for (i = 0; i < tableSize; i++) {
+		FreeList(&table[i]);
+	}
+
+	free(table);
+	table = NULL;
+
+	return SUCCESS;
+}
+
+int FreeList(Node *head)
+{
+	Node *toFree = NULL;
+	Node* tmp = head->next;
+	while(tmp) {
+		toFree = tmp;
+		tmp = tmp->next;
+		free(toFree);
+	}
+
+	head->next = NULL;
+	return SUCCESS;
+}
+
+
+
+int PrintError(char *message)
+{
+	if(message == NULL) {
+		fprintf(stderr, "\nUnknown error occurred!");
+		return SUCCESS;
+	}
+	fprintf(stderr, "\n%s", message);
+
+	return SUCCESS;
+}
+
+int ConsoleInput(const char *message, char *buffer, size_t bufferSize)
+{
+	char *end;
+	if (message != NULL)
+		printf("%s", message);
+	memset(buffer, '\0', bufferSize);
+	fgets(buffer, bufferSize - 1, stdin);
+	end = strchr(buffer, '\n');
+	if (end)
+		*end = '\0';
+
+	return SUCCESS;
+}
+
+int IsNull(int numberOfPointersPassed, char *errorMessage, ...)
+{
+	int i = 0;
+	int isNull = FALSE;
+	va_list args;
+
+	va_start(args, errorMessage);
+	for (i = 0; i < numberOfPointersPassed; i++) {
+		void* ptr = va_arg(args, void *);
+		if (ptr == NULL) {
+			if (errorMessage)
+				fprintf(stderr, "\n%s", errorMessage);
+			return TRUE;
+		}
+	}
+	va_end(args);
+
+	return FALSE;
 }
--- SP_lab_11/SP_lab_11/Source.h
+++ SP_lab_11/SP_lab_11/Source.h
@@ -3,9 +3,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+#include <conio.h> 
 
 #define SUCCESS 0
 #define FAILURE (-1)
-#define BUFFER_LENGTH 1024
 #define TRUE 1
 #define FALSE 0
+#define BUFFER_LENGTH 1024
+#define NAME_LENGTH 128
+#define ID_LENGTH 32
+#define TABLE_SIZE (11)
+#define CHARS_HASHING 5
+
+typedef struct _person {
+	char firstName[NAME_LENGTH];
+	char lastName[NAME_LENGTH];
+	char id[ID_LENGTH];
+} Person;
+
+typedef struct _node {
+	Person *person;
+
+	struct _node *next;
+} Node;
+typedef Node HashTable;
+
+Node *CreateNewNode(Person *person);
+Person *CreateNewPerson(char *firstName, char *lastName, char *id);
+HashTable *CreateHashTable(int tableSize);
+int InitHashTable(HashTable *table, int tableSize);
+int Hash(char *key, int tableSize);
+int HashTableInsert(HashTable *table, int tableSize, Person *toInsert);
+int SortedInsert(Node *listHead, Person *personToInsert);
+int PrintTable(HashTable *table, int tableSize);
+int PrintList(Node *listHead);
+int PrintPerson(Person *p);
+
+int HashTableLookup();
+int HashTableDelete();
+
+int FreeList(Node *head);
+int FreeHashTable(HashTable *table, int tableSize);
+
+int PrintError(char *message);
+int ConsoleInput(const char *message, char *buffer, size_t bufferSize);
+int IsNull(int numberOfPointersPassed, char *errorMessage, ...);
