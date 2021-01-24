3109dedc2d04ea6c7318a148c573621f364ab9e2
MMijoc <markomijoc18@gmail.com>
2021-01-25 00:44:56 +0100

    Removed debugging functions and macros

--- SP_lab_9/SP_lab_9/Source.c
+++ SP_lab_9/SP_lab_9/Source.c
@@ -8,14 +8,9 @@
 #endif
 
 #include "Source.h"
-//For debugging memory leaks
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
 
 int main()
 {
-	//For debugging memory leaks
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	SelectMenu();
 	return SUCCESS;
 }
